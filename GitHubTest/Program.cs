07623d37946a53ea9ffcb3394a7b246bc547b609
Audra Todd <audra_todd@hotmail.com>
2017-05-21 21:31:20 -0400

    This is a test to determine the version control ability of VS Community for Mac

---
+++ GitHubTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHubTest
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
